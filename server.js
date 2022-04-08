2beb46f0f3385f2378077a820e2611757c38a3e6
nblaski <nfb@nicolebruno.com>
2022-04-08 14:02:11 -0400

    serverchange

--- server.js
+++ server.js
@@ -16,10 +16,28 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, process.env.DATABASE_URL, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex : true});
-const db = mongoose.connection;
-db.on('error', error => console.error(error));
-db.once('open', () => console.log('Connected to Mongoose'));
+
+
+try {
+    // Connect to the MongoDB cluster
+     mongoose.connect(
+        process.env.DATABASE_URL,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(" Mongoose is connected")
+    );
+
+  } catch (e) {
+    console.log("could not connect");
+  }
+
+
+
+
+
+// mongoose.connect(process.env.DATABASE_URL, process.env.DATABASE_URL, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex : true});
+// const db = mongoose.connection;
+// db.on('error', error => console.error(error));
+// db.once('open', () => console.log('Connected to Mongoose'));
 
 
 app.use('/', indexRouter)
