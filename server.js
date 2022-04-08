d74f45a97d37cfe817d4a6cfdd53107fcdc1f338
nblaski <nfb@nicolebruno.com>
2022-04-08 13:42:20 -0400

    test11

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 
@@ -15,7 +16,7 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex : true});
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
