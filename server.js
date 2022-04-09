7c0d5db667ed7d9bc660ebd13dacb7b8906e846f
nblaski <nfb@nicolebruno.com>
2022-04-09 01:42:20 -0400

    Initial Author Routes

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
 
@@ -24,7 +27,8 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 
-app.use('/', indexRouter)
+app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 
 var port = process.env.PORT || 3000;
