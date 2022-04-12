4c3f7d443081c5bae770d66f105508766b264dd8
nblaski <nfb@nicolebruno.com>
2022-04-12 17:51:18 -0400

    Delete update and show authors pages

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Author = require('../models/author');
+const Book = require('../models/book');
+
+//New Author Routes
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() });
+});
 
 // All Authors Route
 router.get('/', async (req, res) => {
@@ -19,11 +25,6 @@ router.get('/', async (req, res) => {
     }
 });
 
-//New Author Routes
-router.get('/new', (req, res) => {
-    res.render('authors/new', { author: new Author() });
-});
-
 //Create Author Route
 router.post('/', async (req, res) => {
     const author = new Author({
@@ -31,13 +32,83 @@ router.post('/', async (req, res) => {
     });
     try {
         const newAuthor = await author.save();
-        // res.redirect(`authors/${newAuthor.id}`);
-        res.redirect(`authors`);
+        res.redirect(`authors/${author.id}`);
+        // res.redirect(`authors`);
     } catch {
         res.render('authors/new', {
             author: author,
             errorMessage: 'Error creating Author'
         })
+    }
+});
+
+//GET Show ROUTE
+//: is will be a variable id passed into the request
+// req.params.id  gives the params of the url passed in from /:id
+router.get('/:id', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id);
+        const books = await Book.find({ author: author.id }).limit(6).exec();
+        res.render('authors/show', {
+            author: author,
+            booksByAuthor: books
+        })
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    }
+});
+
+//EDIT ROUTE
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id);
+        res.render('authors/edit', { author: author });
+    } catch {
+        res.redirect('/authors');
+    }
+});
+
+//Need to install library to make put() and delete() requests called method-override
+
+//UPDATE ROUTE
+//in REST you use put() instead of post() to update routes
+router.put('/:id', async (req, res) => {
+    //need to define author variable outside the try block so the catch can access it
+    let author;
+    try {
+        author = await Author.findById(req.params.id);
+        author.name = req.body.name;
+        await author.save();
+        res.redirect(`/authors/${author.id}`);
+    } catch {
+        if (author == null) {
+            console.log("Author equals null");
+            res.redirect('/');
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error Updating Author'
+            })
+        } 
+    }
+});
+
+//DELETE ROUTE
+//REST uses delete()
+router.delete('/:id', async (req, res) => {
+    let author;
+    try {
+        author = await Author.findById(req.params.id);
+        await author.remove();
+        res.redirect('/authors');
+    } catch {
+        if (author == null) {
+            console.log("Author equals null");
+            res.redirect('/');
+        } else {
+            res.redirect(`/authors/${author.id}`)
+        } 
     }
 });
 
